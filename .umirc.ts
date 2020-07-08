6aef9d046e325b520ac62d19a495fb9ecaa5c6c5
23233 <1@qq.com>
2020-07-08 23:25:45 +0800

    add title show

--- .umirc.ts
+++ .umirc.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     type: 'none',
   },
   routes: Routes,
-
+  title: false,
   proxy: {
     '/api/': {
       target: 'http://127.0.0.1:8080',
--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
 import './index.less';
+import { useModel, Helmet } from 'umi';
 
 
 export default (props) => {
+  const { config } = useModel('useAuthModel');
+
   return (
     <ConfigProvider locale={zhCN}>
+      <Helmet encodeSpecialCharacters={false}>
+        <title>{config?.name || '后台管理系统'}</title>
+      </Helmet>
       {props.children}
     </ConfigProvider>
   );
