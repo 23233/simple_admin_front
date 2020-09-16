80b63cfb4d1d4a9fe799175161c8e5cacc340b7f
1 <1@qq.com>
2020-09-16 12:32:23 +0800

    修复bool变更错误的问题

--- src/pages/dataShow/dataForm.js
+++ src/pages/dataShow/dataForm.js
@@ -1,17 +1,39 @@
 import React, { useState } from 'react';
 import Moment from 'moment';
-import { Button, Modal, Form, Input, Radio, InputNumber, Switch, TimePicker, DatePicker, Select, Spin } from 'antd';
+import {
+  Button,
+  Modal,
+  Form,
+  Input,
+  Radio,
+  InputNumber,
+  Switch,
+  TimePicker,
+  DatePicker,
+  Select,
+  Spin,
+} from 'antd';
 import Tools from '../../utils/tools';
 import { useRequest } from '@umijs/hooks';
 import req from '../../utils/url';
 
-
 const { Option } = Select;
 
-const CollectionCreateForm = ({ onCreate, onCancel, fieldsList, initValues, loading, isAction }) => {
+const CollectionCreateForm = ({
+  onCreate,
+  onCancel,
+  fieldsList,
+  initValues,
+  loading,
+  isAction,
+}) => {
   const [form] = Form.useForm();
 
-  const { run: searchText, data: searchData, loading: searchLoading } = useRequest(req.searchRouterData, {
+  const {
+    run: searchText,
+    data: searchData,
+    loading: searchLoading,
+  } = useRequest(req.searchRouterData, {
     manual: true,
     debounceInterval: 500,
   });
@@ -24,15 +46,31 @@ const CollectionCreateForm = ({ onCreate, onCancel, fieldsList, initValues, load
         initialValues[d.map_name] = Moment(initValues[d.map_name]);
       } else if (['bool'].includes(d.types)) {
         initialValues[d.map_name] = !!parseInt(initValues[d.map_name] || false);
-      } else if (['uint', 'uint8', 'uint16', 'uint32', 'uint64', 'int', 'int8', 'int16', 'int32', 'int64', 'float32', 'float64', 'time.Duration'].includes(d.types)) {
+      } else if (
+        [
+          'uint',
+          'uint8',
+          'uint16',
+          'uint32',
+          'uint64',
+          'int',
+          'int8',
+          'int16',
+          'int32',
+          'int64',
+          'float32',
+          'float64',
+          'time.Duration',
+        ].includes(d.types)
+      ) {
         initialValues[d.map_name] = initValues[d.map_name] || 0;
       }
       // 再判断类型
       const sp_tags = Tools.parseTags(d.sp_tags);
       if (sp_tags && sp_tags.length) {
         // 先判断是否有外键
-        let fk = sp_tags.find((e) => e.key === 'fk');
-        let multiple = sp_tags.find((e) => e.key === 'multiple');
+        let fk = sp_tags.find(e => e.key === 'fk');
+        let multiple = sp_tags.find(e => e.key === 'multiple');
         if (fk && multiple) {
           initialValues[d.map_name] = initValues[d.map_name]?.split(',') || [];
         }
@@ -45,14 +83,14 @@ const CollectionCreateForm = ({ onCreate, onCancel, fieldsList, initValues, load
         if (d.sp_tags) {
           // 解析出数据
           const tags = Tools.parseTags(d.sp_tags);
-          tags.map((t) => {
+          tags.map(t => {
             switch (t.key) {
               // 数据关联
               case 'lineTo':
                 initialValues[d.map_name] = initValues[t.value];
                 break;
               case 'fk':
-                let multiple = tags.find((e) => e.key === 'multiple');
+                let multiple = tags.find(e => e.key === 'multiple');
                 if (multiple) {
                   initialValues[d.map_name] = initValues[t.value].split(',');
                 }
@@ -64,21 +102,19 @@ const CollectionCreateForm = ({ onCreate, onCancel, fieldsList, initValues, load
         }
       });
     }
-
   }
 
   console.log('initialValues', initialValues, fieldsList);
 
-
-  const TypeToElement = (k) => {
+  const TypeToElement = k => {
     // console.log('type to element', k);
     const t = k.types;
     // 先匹配类型
     const sp_tags = Tools.parseTags(k.sp_tags);
     if (sp_tags && sp_tags.length) {
       // 先判断是否有外键
-      let fk = sp_tags.find((e) => e.key === 'fk');
-      let multiple = sp_tags.find((e) => e.key === 'multiple');
+      let fk = sp_tags.find(e => e.key === 'fk');
+      let multiple = sp_tags.find(e => e.key === 'multiple');
       if (fk) {
         return (
           <Select
@@ -88,17 +124,21 @@ const CollectionCreateForm = ({ onCreate, onCancel, fieldsList, initValues, load
             filterOption={false}
             defaultActiveFirstOption={false}
             onFocus={() => searchText(fk.value, null)}
-            onSearch={(value) => searchText(fk.value, value)}
-            notFoundContent={searchLoading ? <Spin size="small"/> : null}
+            onSearch={value => searchText(fk.value, value)}
+            notFoundContent={searchLoading ? <Spin size="small" /> : null}
             placeholder="请输入筛选条件"
             optionLabelProp={'id'}
             allowClear
           >
-            {searchData && searchData.length && searchData.map((d, i) => {
-              return (
-                <Option key={d.id} value={d.id}>{JSON.stringify(d)}</Option>
-              );
-            })}
+            {searchData &&
+              searchData.length &&
+              searchData.map((d, i) => {
+                return (
+                  <Option key={d.id} value={d.id}>
+                    {JSON.stringify(d)}
+                  </Option>
+                );
+              })}
           </Select>
         );
       }
@@ -107,7 +147,7 @@ const CollectionCreateForm = ({ onCreate, onCancel, fieldsList, initValues, load
       case 'float32':
       case 'float64':
       case 'int':
-      case 'int8' :
+      case 'int8':
       case 'int16':
       case 'int32':
       case 'int64':
@@ -118,42 +158,51 @@ const CollectionCreateForm = ({ onCreate, onCancel, fieldsList, initValues, load
       case 'uint64':
       case 'time.Duration':
         return (
-          <InputNumber style={{ width: '100%' }} placeholder={'please input value'}/>
+          <InputNumber
+            style={{ width: '100%' }}
+            placeholder={'please input value'}
+          />
         );
       case 'time':
       case 'time.Time':
-        return (
-          <DatePicker showTime/>
-        );
+        return <DatePicker showTime />;
       case 'bool':
-        return (
-          <Switch/>
-        );
+        return <Switch />;
       default:
-        return (
-          <Input placeholder={'please input value'}/>
-        );
+        return <Input placeholder={'please input value'} />;
     }
-
-
   };
 
-  const MomentToFormat = (values) => {
+  const MomentToFormat = values => {
     Object.keys(values).map((k, i) => {
       const v = values[k];
-      if (typeof (v) === 'object' && v._isAMomentObject) {
+      if (typeof v === 'object' && v._isAMomentObject) {
         values[k] = v.format('YYYY-MM-DD HH:mm:ss');
       }
     });
   };
 
-  const ValuesTypeChange = (values) => {
+  const ValuesTypeChange = values => {
     Object.keys(values).map((k, i) => {
       const v = values[k];
-      const t = fieldsList.fields.find((d) => d.map_name === k);
+      const t = fieldsList.fields.find(d => d.map_name === k);
       if (['bool'].includes(t.types)) {
-        values[k] = !!parseInt(v);
-      } else if (['uint', 'uint8', 'uint16', 'uint32', 'uint64', 'int', 'int8', 'int16', 'int32', 'int64', 'time.Duration'].includes(t.types)) {
+        values[k] = !!v;
+      } else if (
+        [
+          'uint',
+          'uint8',
+          'uint16',
+          'uint32',
+          'uint64',
+          'int',
+          'int8',
+          'int16',
+          'int32',
+          'int64',
+          'time.Duration',
+        ].includes(t.types)
+      ) {
         values[k] = Number(v);
       } else if (['float32', 'float64'].includes(t.types)) {
         values[k] = Number(v);
@@ -170,7 +219,6 @@ const CollectionCreateForm = ({ onCreate, onCancel, fieldsList, initValues, load
         MomentToFormat(values);
         // 格式转换
         ValuesTypeChange(values);
-        console.log('values', values);
         onCreate(values);
       })
       .catch(info => {
@@ -185,7 +233,7 @@ const CollectionCreateForm = ({ onCreate, onCancel, fieldsList, initValues, load
       okText="提交"
       cancelText="取消"
       onCancel={onCancel}
-      bodyStyle={{maxHeight:"60vh",overflow:"hidden auto"}}
+      bodyStyle={{ maxHeight: '60vh', overflow: 'hidden auto' }}
       confirmLoading={loading}
       onOk={runCreate}
     >
@@ -195,34 +243,41 @@ const CollectionCreateForm = ({ onCreate, onCancel, fieldsList, initValues, load
         layout={'horizontal'}
         initialValues={initialValues}
       >
-        {
-          fieldsList.fields.map((k, i) => {
-            const isPass = k.map_name !== fieldsList.autoincr && k.map_name !== fieldsList.updated && k.map_name !== fieldsList.deleted && k.map_name !== fieldsList.version && k.xorm_tags.split(' ').includes('created') === false && k.sp_tags.split(' ').includes('autogen') === false;
-            if (isPass) {
-              let required = false;
-              if (k.xorm_tags.split(' ').includes('notnull') || k.xorm_tags.split(' ').includes('not null')) {
-                required = true;
-              }
-              return (
-                <Form.Item
-                  key={`form_${i}`}
-                  name={k.map_name}
-                  label={k?.comment_tags || k.map_name}
-                  valuePropName={['bool'].includes(k.types) ? 'checked' : 'value'}
-                  rules={[
-                    {
-                      required: required,
-                      message: 'please input data',
-                    },
-                  ]}
-                >
-                  {TypeToElement(k)}
-                </Form.Item>
-              );
+        {fieldsList.fields.map((k, i) => {
+          const isPass =
+            k.map_name !== fieldsList.autoincr &&
+            k.map_name !== fieldsList.updated &&
+            k.map_name !== fieldsList.deleted &&
+            k.map_name !== fieldsList.version &&
+            k.xorm_tags.split(' ').includes('created') === false &&
+            k.sp_tags.split(' ').includes('autogen') === false;
+          if (isPass) {
+            let required = false;
+            if (
+              k.xorm_tags.split(' ').includes('notnull') ||
+              k.xorm_tags.split(' ').includes('not null')
+            ) {
+              required = true;
             }
-
-          })
-        }
+            const valueProp = ['bool'].includes(k.types) ? 'checked' : 'value';
+            return (
+              <Form.Item
+                key={`form_${i}`}
+                name={k.map_name}
+                label={k?.comment_tags || k.map_name}
+                valuePropName={valueProp}
+                rules={[
+                  {
+                    required: required,
+                    message: 'please input data',
+                  },
+                ]}
+              >
+                {TypeToElement(k)}
+              </Form.Item>
+            );
+          }
+        })}
       </Form>
     </Modal>
   );
