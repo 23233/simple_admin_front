8c30f16a64c1e4de0ed659e505fa8775c9d91c5d
1 <1@qq.com>
2020-10-12 11:41:34 +0800

    1.显示调整

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,6 +1,17 @@
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import React, { useState, useEffect } from 'react';
-import { Card, message, Button, Tabs, Typography, Popover, Input, Spin, Row, Col } from 'antd';
+import {
+  Card,
+  message,
+  Button,
+  Tabs,
+  Typography,
+  Popover,
+  Input,
+  Spin,
+  Row,
+  Col,
+} from 'antd';
 import { useRequest } from 'ahooks';
 import req from '@/utils/url';
 import { EditOutlined } from '@ant-design/icons';
@@ -19,50 +30,62 @@ export default function(props) {
   const [chatList, setChatList] = useState([]);
 
   // 获取屏幕
-  const { loading: getScreenLoading, data: screens, run: getScreen } = useRequest(req.dashBoardScreenGet, {
-    onSuccess: (resp) => {
+  const {
+    loading: getScreenLoading,
+    data: screens,
+    run: getScreen,
+  } = useRequest(req.dashBoardScreenGet, {
+    onSuccess: resp => {
       if (resp && resp.length) {
-        const d = resp.find((d) => d.is_default);
+        const d = resp.find(d => d.is_default);
         setShowScreen(d?.id || resp[0].id);
       }
     },
   });
   // 修改或新增
-  const { loading: editOrAddLoading, run: editOrAddReq } = useRequest(req.dashBoardScreenAddOrEdit, {
-    manual: true,
-    onSuccess: () => {
-      getScreen();
+  const { loading: editOrAddLoading, run: editOrAddReq } = useRequest(
+    req.dashBoardScreenAddOrEdit,
+    {
+      manual: true,
+      onSuccess: () => {
+        getScreen();
+      },
     },
-  });
+  );
   // 删除
-  const { run: deleteReq, loading: deleteLoading } = useRequest(req.dashBoardScreenDelete, {
-    manual: true, onSuccess: () => {
-      getScreen();
+  const { run: deleteReq, loading: deleteLoading } = useRequest(
+    req.dashBoardScreenDelete,
+    {
+      manual: true,
+      onSuccess: () => {
+        getScreen();
+      },
     },
-  });
+  );
 
   // 获取图表列表
-  const { loading: getChatsLoading, run: getChats } = useRequest(req.dashBoardGet, {
-    manual: true,
-    onSuccess: (resp) => {
-      if (!resp.status) {
-        resp.map((d) => {
-          d.config = JSON.parse(d.config);
-          d.data_source = JSON.parse(d.data_source);
-          return d;
-        });
-        setChatList(resp);
-      }
+  const { loading: getChatsLoading, run: getChats } = useRequest(
+    req.dashBoardGet,
+    {
+      manual: true,
+      onSuccess: resp => {
+        if (!resp.status) {
+          resp.map(d => {
+            d.config = JSON.parse(d.config);
+            d.data_source = JSON.parse(d.data_source);
+            return d;
+          });
+          setChatList(resp);
+        }
+      },
     },
-  });
-
+  );
 
   useEffect(() => {
     if (showScreen) {
       getChats(showScreen);
     }
   }, [showScreen]);
-
 
   // 屏幕当前选中变更
   const screenChange = value => {
@@ -73,7 +96,6 @@ export default function(props) {
   const flushData = () => {
     getChats(showScreen);
   };
-
 
   // 屏幕操作
   const screenEdit = (targetKey, action) => {
@@ -94,7 +116,6 @@ export default function(props) {
     editOrAddReq({ name: value, id: parseInt(id) });
   };
 
-
   return (
     <PageHeaderWrapper content="用户仪表台">
       <Card>
@@ -105,56 +126,63 @@ export default function(props) {
             activeKey={showScreen.toString()}
             onEdit={screenEdit}
             tabBarExtraContent={
-              screens && showScreen ?
+              screens && showScreen ? (
                 <React.Fragment>
                   <Button onClick={flushData}>刷新</Button>
                   <Link to={ROUTERS.dashBoardAdd + '?id=' + showScreen}>
-                    <Button type={'primary'} title={'新增图表'}>新增图表</Button>
+                    <Button type={'primary'} title={'新增图表'}>
+                      新增图表
+                    </Button>
                   </Link>
-                </React.Fragment> : null
+                </React.Fragment>
+              ) : null
             }
           >
-            {(screens || []).map((item, index) => (
-              <TabPane
-                tab={
-                  <React.Fragment>
-                    <span>{item.name}</span>
-                    <Popover
-                      title={'变更名称'}
-                      trigger={'click'}
-                      content={
-                        <Search
-                          placeholder={'请输入新名称'}
-                          enterButton={'确定'}
-                          maxLength={20}
-                          allowClear
-                          onSearch={value => screenNameEdit(item.id, value)}
-                        />
-                      }
-                    >
-                      <EditOutlined style={{ margin: '0 0 0 10px' }}/>
-                    </Popover>
-                  </React.Fragment>
-                }
-                key={item.id}
-                closable={index >= 1}
-              />
-            ))}
+            {screens && screens?.length
+              ? screens.map((item, index) => (
+                  <TabPane
+                    tab={
+                      <React.Fragment>
+                        <span>{item.name}</span>
+                        <Popover
+                          title={'变更名称'}
+                          trigger={'click'}
+                          content={
+                            <Search
+                              placeholder={'请输入新名称'}
+                              enterButton={'确定'}
+                              maxLength={20}
+                              allowClear
+                              onSearch={value => screenNameEdit(item.id, value)}
+                            />
+                          }
+                        >
+                          <EditOutlined style={{ margin: '0 0 0 10px' }} />
+                        </Popover>
+                      </React.Fragment>
+                    }
+                    key={item.id}
+                    closable={index >= 1}
+                  />
+                ))
+              : null}
           </Tabs>
         </Spin>
         <Spin spinning={getChatsLoading}>
           <Row>
-            {
-              chatList.map((d, i) => {
-                return (
-                  <Col key={d.id} xs={24} sm={12} md={8} xl={6}>
-                    <SingleChart data={d} screenId={showScreen} run_flush={flushData} delay={200 * (i + 1)}/>
-                  </Col>
-                );
-              })
-            }
+            {chatList.map((d, i) => {
+              return (
+                <Col key={d.id} xs={24} sm={12} md={8} xl={6}>
+                  <SingleChart
+                    data={d}
+                    screenId={showScreen}
+                    run_flush={flushData}
+                    delay={200 * (i + 1)}
+                  />
+                </Col>
+              );
+            })}
           </Row>
-
         </Spin>
       </Card>
     </PageHeaderWrapper>
